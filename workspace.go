d3a4df0614aa07d1163909a5b065e77542c15805
Kingmidas74 <kingmidas1992@gmail.com>
2021-08-03 18:56:11 +0300

    stable canvas animation

--- workspace.go
+++ workspace.go
@@ -4,51 +4,53 @@ import (
 	g "github.com/AllenDang/giu"
 	"github.com/Kingmidas74/gonesis_engine/contracts"
 	"image"
+	"time"
 )
 
 const (
 	title string = "Gonesis"
 )
 
-
 type Workspace struct {
-	Width int
+	Width  int
 	Height int
 
 	currentWindow *g.MasterWindow
 
-	texture *g.Texture
+	texture      *g.Texture
 	currentWorld contracts.IWorld
 }
 
-func(this *Workspace) Init(world contracts.IWorld) {
+func (this *Workspace) Init(world contracts.IWorld) {
 	this.currentWorld = world
 	this.currentWindow = g.NewMasterWindow(title, this.Width, this.Height, g.MasterWindowFlagsNotResizable)
 }
 
-func(this *Workspace) Start() {
+func (this *Workspace) Start() {
 	this.currentWindow.Run(this.loop)
 }
 
-func(this* Workspace) runEvolution() {
+func (this *Workspace) runEvolution() {
 
-	this.currentWorld.Action(1, func(terrain contracts.ITerrain, currentDay int) {
-		img := DrawFrame(terrain,100)
-		go func() {
+	go this.currentWorld.Action(1, func(terrain contracts.ITerrain, currentDay int) {
+		img := DrawFrame(terrain, 100)
+
+		defer time.AfterFunc(time.Duration(1)*time.Second, func() {
 			this.texture, _ = g.NewTextureFromRgba(img)
-		}()
+		}).Stop()
 
+		time.Sleep(1 * time.Second)
 	})
 }
 
-func(this *Workspace) drawControls() *g.Layout {
+func (this *Workspace) drawControls() *g.Layout {
 	layout := g.Layout{}
 	layout = append(layout, g.Label("Hello there"),
 		g.Button("Start simulation").OnClick(this.runEvolution))
 	return &layout
 }
 
-func(this *Workspace) drawCanvas() *g.Layout {
+func (this *Workspace) drawCanvas() *g.Layout {
 	layout := g.Layout{}
 	layout = append(layout,
 		g.Custom(func() {
@@ -61,10 +63,9 @@ func(this *Workspace) drawCanvas() *g.Layout {
 	return &layout
 }
 
-func(this *Workspace) loop() {
+func (this *Workspace) loop() {
 	controls := this.drawControls()
 	canvas := this.drawCanvas()
 
-	g.SingleWindow().Layout(g.SplitLayout(g.DirectionHorizontal,true,100,controls,canvas))
-
+	g.SingleWindow().Layout(g.SplitLayout(g.DirectionHorizontal, true, 100, controls, canvas))
 }
